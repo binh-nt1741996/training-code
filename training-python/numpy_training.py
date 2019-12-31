8f09abd6d9e77bdb3572367a92763f2d59fe2dfa
binh-nt1741996 <binh.nt1741996@gmail.com>
2019-12-31 16:45:54 +0900

    Add matplotlib

--- training-python/numpy_training.py
+++ training-python/numpy_training.py
@@ -1,2 +1,6 @@
 import numpy as np
-print("This is my first file")
+
+if __name__=="__main__":
+    inp_data = np.array(list(map(int, input("Enter list of integers: ").split(" "))))
+    data1 = np.array([1,3,4])
+    print(np.concatenate([np.repeat(inp_data,3),np.tile(inp_data,3)]))
