66b29aae664b49e43acb5567f78d21a6ab04d7ac
Binh Nguyen <binh.nt1741996@gmail.com>
2020-01-01 18:43:55 +0900

    Update New Year

--- training-python/matplotlib_training.py
+++ training-python/matplotlib_training.py
@@ -1,1 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+x = np.arange(20)
+plt.plot(x,x*2,label="line 1", ls=":")
+plt.plot(x,x*3,label="line 2", ls="--")
+
+plt.legend()
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+plt.title("Sample graph")
+plt.show()
