39eab5464dfcb97c70597250e602f6de702b6b64
BN174281 <binh.nt1741996@gmail.com>
2020-05-19 15:08:10 +0900

    Update Java

--- training-java/ex_headfirst/Of76.java
+++ training-java/ex_headfirst/Of76.java
@@ -1,29 +1,13 @@
-public class Of76 extends Clowns{
+/**
+ * Of76
+ */
+import java.util.Date;
+
+public class Of76 {
+    int i;
+    Integer j;
     public static void main(String[] args) {
-        Nose[] i = new Nose[3];
-        i[0] = new Acts();
-        i[1] = new Clowns();
-        i[2] = new Of76();
-        for (int x = 0; x < 3; x++){
-            System.out.println(i[x].iMethod() + " " + i[x].getClass());
-        }       
-    }
-}
-
-interface Nose{
-    public int iMethod();
-}
-
-abstract class Picasso implements Nose{
-    public int iMethod(){
-        return 7;
-    }
-}
-
-class Clowns extends Picasso{}
-
-class Acts implements Nose{
-    public int iMethod(){
-        return 5;
+        Date today = new Date();
+        System.out.println(String.format("Today is %tA/%<tB/%<td, and the time is %<tr", today));
     }
 }
